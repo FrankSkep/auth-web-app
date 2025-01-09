aca0f39305e26487d32daeacdc8c70ad7e70770f
FrankSkep <f.cornejo.pacheco@gmail.com>
2025-01-08 22:17:58 -0800

    unified LoginController - RegistrationController

---
+++ src/main/java/com/authapp/controller/AuthController.java
@@ -0,0 +1,51 @@
+package com.authapp.controller;
+
+import com.authapp.dto.RegisterDTO;
+import com.authapp.service.UserService;
+import jakarta.validation.Valid;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+@Controller
+public class AuthController {
+    private final UserService userService;
+
+    public AuthController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping("/login")
+    public String login() {
+        return "login";
+    }
+
+    @GetMapping("/register")
+    public String showRegistrationForm(Model model) {
+        model.addAttribute("user", new RegisterDTO());
+        return "register";
+    }
+
+    @PostMapping("/register")
+    public String registerUser(@ModelAttribute("user") @Valid RegisterDTO registrationDto,
+                               BindingResult bindingResult, RedirectAttributes redirectAttributes) {
+
+        if (bindingResult.hasErrors()) {
+            return "register";
+        }
+
+        try {
+            userService.registerUser(registrationDto);
+            redirectAttributes.addFlashAttribute("successMsg", "Successfully registered.");
+            return "redirect:/login?success";
+        } catch (
+                IllegalArgumentException e) {
+            redirectAttributes.addFlashAttribute("errorMsg", e.getMessage());
+            return "redirect:/register?error";
+        }
+    }
+}
