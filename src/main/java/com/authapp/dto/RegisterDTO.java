30fb4f2b39ab3d647ad600bad28b83607d523c2d
FrankSkep <f.cornejo.pacheco@gmail.com>
2025-01-08 22:27:05 -0800

    implement validations and show in templates

--- src/main/java/com/authapp/dto/RegisterDTO.java
+++ src/main/java/com/authapp/dto/RegisterDTO.java
@@ -1,5 +1,7 @@
 package com.authapp.dto;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -10,8 +12,17 @@ import lombok.Setter;
 @AllArgsConstructor
 @NoArgsConstructor
 public class RegisterDTO {
+
+    @NotBlank(message = "Username cannot be empty")
+    @Size(min = 3, max = 20, message = "Username must be between 3 and 20 characters")
     private String username;
+
+    @NotBlank(message = "Password cannot be empty")
+    @Size(min = 6, message = "Password must be at least 6 characters")
     private String password;
+
+    @NotBlank(message = "Password confirmation cannot be empty")
+    @Size(min = 6, message = "Password confirmation must be at least 6 characters")
     private String confirmPassword;
 }
 
